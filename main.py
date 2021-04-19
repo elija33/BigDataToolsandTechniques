e2ced79163ed5b4845db99c0c2b44d36aff60fbb
Elija Amponsah <amponsaeli@gmail.com>
2021-04-19 15:15:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,5 +14,5 @@ st.title("Stock Prediction App")
 stocks = ("AAPL", "GOOG", "MSFT", "GME")
 selected_stocks = st.selectbox("Select dataset for prediction", stocks)
 
-n_years = st.slider("Years of prediction:", 1 , 5)
+n_years = st.slider("Years of prediction:", 1 , 4)
 period = n_years * 365
